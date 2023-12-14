524035195c316a772d685f7eff632a62bc6aedd3
ml1908 <ml1908@gmx.at>
2023-12-14 09:59:05 +0100

    npm Uebungen

---
+++ npm/index.mjs
@@ -0,0 +1,9 @@
+﻿import { logRed, logGreen, logBlue, logYellow } from './logger.mjs'
+
+let text = 'Servus Erdnuss';
+
+logRed(text);
+logGreen(text);
+logBlue(text);
+logYellow(text);
+
---
+++ npm/logger.mjs
@@ -0,0 +1,36 @@
+﻿import chalk from 'chalk';
+
+
+
+let text = 'Wie gehts dir'
+function logRed(text){
+    console.log(chalk.red(text));
+}
+function logGreen(text){
+    console.log(chalk.green(text));
+}
+
+function logBlue(text){
+    console.log(chalk.blue(text));
+}
+
+function logYellow(text){
+    console.log(chalk.yellow(text));
+}
+
+logRed(text)
+logGreen(text)
+logBlue(text)
+logYellow(text)
+
+
+export { logRed, logGreen, logBlue, logYellow };
+
+
+
+const name = 'Michael';
+console.log(chalk.red('Hello %s'), name);
+console.log(chalk.blue('Hello ') + name + chalk.red('!'));
+console.log(chalk.green('How are u?'))
+
+console.log(chalk.green('Merry Christmas'))
